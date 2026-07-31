7b1cdcbc8dafc08264432627e5213c15a35be578
jeylofton <jeylofton@gmail.com>
2026-07-31 05:15:33 -0500

    New Class Day 1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+function App() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer(){
+
+    return(
+        <div>
+            <p>This is my Online Store</p>
+        </div>
+    );
+}
+
+export default Footer;
